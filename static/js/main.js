8ee1f7a1ee0c4f8b8f8dae5ee3fd18640db1da7c
Haruka Goto <39117456+haru6999@users.noreply.github.com>
2019-11-03 00:10:40 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -90,5 +90,5 @@ var page = 0; //ページ
 var srcName = "../png/01/" + page + ".png";
 document.getElementById('toNext').addEventListener('click', function () {
   page++;
-  elem.contentWindow.document.getElementById('bookImage').src = '"' + srcName + '"';
+  elem.contentWindow.document.getElementById('bookImage').src = srcName ;
 }, false);
