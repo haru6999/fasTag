753f92618e8698c07387b706dec28bc590665006
moscwa <ymatsubayashi09@outlook.jp>
2019-11-02 21:55:28 +0900

    :star:

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
-function Send(path,dic) {
+function Send(path, dic) {
   let req = new XMLHttpRequest();
   req.onreadystatechange = function () {
     if (req.readyState == 4) { // 通信の完了時
       if (req.status == 200) { // 通信の成功時
         console.log('done!')
-        MakePin(eval("("+req.responseText+")"))
+        MakePin(eval("(" + req.responseText + ")"))
       }
     } else {
       console.log('connecting....')
@@ -13,19 +13,19 @@ function Send(path,dic) {
 
   keys = Object.keys(dic)
   vals = Object.values(dic)
-  output=""
+  output = ""
   for (i = 0; i < keys.length; i++) {
     if (i) {
       output += "&"
     }
     output += "" + keys[i] + "=" + vals[i]
   }
-  req.open('GET', '/'+path+'?' + output, true);
+  req.open('GET', '/' + path + '?' + output, true);
   // req.send(null);
 }
 
 
-function MakePin(res){
+function MakePin(res) {
   console.log(res)
 }
 
@@ -41,52 +41,55 @@ function MakePin(res){
 
 // 画像のクリック場所取得
 const elem = document.getElementById('bookIFrame');
-console.log(elem)
-const bookImage = elem.contentWindow.document.querySelector('#bookImage');
-console.log(bookImage)
 
-// let target = document.getElementById('bb');
-let elementX = document.getElementById('bb').clientWidth; //要素の横幅
-let elementY = document.getElementById('bb').clientHeight; //要素の縦幅
-const arrayX = [];
-const arrayY = [];
-let gridX = 35;
-let gridY = 50;
+setTimeout(() => {
+  console.log('ok')
+  var bookImage = elem.contentWindow.document.getElementById('bookImage')
+  console.log(bookImage)
 
-for (var i = 0; i < gridX; i++) {
-  arrayX[i] = elementX / gridX * (i+1);
-}
-for (var j = 0; j < gridY; j++) {
-  arrayY[j] = elementY / gridY * (j+1);
-}
 
-target.addEventListener('click', function(e){
-  let targetX = e.offsetX;
-  let targetY = e.offsetY;
-  let gridArrayX = 0;
-  let gridArrayY = 0;
-  // console.log(targetX,targetY);
-  // console.log(elementX,elementY);
+  let target = bookImage
+  let elementX = bookImage.clientWidth; //要素の横幅
+  let elementY = bookImage.clientHeight; //要素の縦幅
+  const arrayX = [];
+  const arrayY = [];
+  let gridX = 35;
+  let gridY = 50;
+
   for (var i = 0; i < gridX; i++) {
-    if(targetX >= arrayX[i]){
-      gridArrayX ++;
-    }
+    arrayX[i] = elementX / gridX * (i + 1);
   }
-  for (var i = 0; i < gridY; i++) {
-    if(targetY >= arrayY[i]){
-      gridArrayY ++;
-    }
+  for (var j = 0; j < gridY; j++) {
+    arrayY[j] = elementY / gridY * (j + 1);
   }
 
-  Send('push',{"x":gridArrayX,"y":gridArrayY,"tagInfo":"hello","attribute":"memo","page":0,"user_id":"000000000","book_id":"000000000"})
+  target.addEventListener('click', function (e) {
+    let targetX = e.offsetX;
+    let targetY = e.offsetY;
+    let gridArrayX = 0;
+    let gridArrayY = 0;
+    // console.log(targetX,targetY);
+    // console.log(elementX,elementY);
+    for (var i = 0; i < gridX; i++) {
+      if (targetX >= arrayX[i]) {
+        gridArrayX++;
+      }
+    }
+    for (var i = 0; i < gridY; i++) {
+      if (targetY >= arrayY[i]) {
+        gridArrayY++;
+      }
+    }
 
-});
+    Send('push', { "x": gridArrayX, "y": gridArrayY, "tagInfo": "hello", "attribute": "memo", "page": 0, "user_id": "000000000", "book_id": "000000000" })
 
+  });
 
+}, 1000)
 // 画像移動
 var page = 0; //ページ
-document.getElementById('toNext').addEventListener('click', function() {
+document.getElementById('toNext').addEventListener('click', function () {
   console.log('bookImage');
-  page ++;
-  document.getElementById('bb').src="../static/png/01/"+page+".png";
+  page++;
+  document.getElementById('bookImage').src = "../static/png/01/" + page + ".png";
 }, false);
